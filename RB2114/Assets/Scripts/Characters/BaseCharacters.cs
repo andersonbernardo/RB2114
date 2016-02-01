0cec1ad5ba8fb2f5bf2f7c033c0d618efd36f658
anderson.bernardo1@gmail.com <anderson.bernardo1@gmail.com>
2016-02-01 00:25:35 -0200

    organizando pastas

---
+++ RB2114/Assets/Scripts/Characters/BaseCharacters.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseCharacters : MonoBehaviour {
+
+    public string CharacterName { get; set; }
+    public string CharacterDescription { get; set; }
+
+    public int CharacterAgility { get; set; }
+    public int CharacterStamina { get; set; }
+    public int CharacterStrength { get; set; }
+}
